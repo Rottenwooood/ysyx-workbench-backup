a49e51d843322bbed1cbd05fdd79983784caa5d1
tracer-ysyx <tracer@ysyx.org>
2026-08-11 11:08:43 +0800

    > sim RTL ysyx_22040000 李晓琛 Linux c6h4o2-ASUS-TUF-Gaming-A15-FA507RM-FA507RM 7.0.0-28-generic #28~24.04.1-Ubuntu SMP PREEMPT_DYNAMIC Wed Jul  1 15:50:57 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  11:08:43 up  1:46,  1 user,  load average: 0.03, 0.29, 0.44

--- npc/vsrc/top.v
+++ npc/vsrc/top.v
@@ -3,10 +3,22 @@ module top(
   input rst,
   output reg[15:0] ledr
 );
+reg [3:0] cnt;
 always@(posedge clk)begin
-	if(rst || ledr == 16'b1 << 15)
+	if(rst)begin
+		cnt <= 0;
 		ledr <= 16'b1;
-	else
-		ledr <= ledr << 1;
+	end
+	else begin
+		cnt <= cnt + 1;
+		if(cnt == 4'b1111)begin
+			if(ledr == 16'b1 << 15)
+				ledr <= 16'b1;
+			else if(ledr == 0)
+				ledr <= 16'b1;
+			else
+				ledr <= ledr << 1;
+		end
+	end
 end
 endmodule
