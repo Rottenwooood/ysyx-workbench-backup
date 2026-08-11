8784eb6f5888f7371c52e95f1bee5757c3d307bd
tracer-ysyx <tracer@ysyx.org>
2026-08-11 10:48:45 +0800

    > sim RTL ysyx_22040000 李晓琛 Linux c6h4o2-ASUS-TUF-Gaming-A15-FA507RM-FA507RM 7.0.0-28-generic #28~24.04.1-Ubuntu SMP PREEMPT_DYNAMIC Wed Jul  1 15:50:57 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  10:48:45 up  1:26,  1 user,  load average: 0.79, 0.80, 0.75

--- npc/vsrc/top.v
+++ npc/vsrc/top.v
@@ -1,10 +1,3 @@
-// DESCRIPTION: Verilator: Verilog example module
-//
-// This file ONLY is placed under the Creative Commons Public Domain.
-// SPDX-FileCopyrightText: 2017 Wilson Snyder
-// SPDX-License-Identifier: CC0-1.0
-
-// See also https://verilator.org/guide/latest/examples.html"
 module top(
   input clk,
   input rst,
