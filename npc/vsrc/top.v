2fb2ca147f9a9207f0e49da9cb38a442ed430f0a
tracer-ysyx <tracer@ysyx.org>
2026-08-11 11:15:53 +0800

    > sim RTL ysyx_22040000 李晓琛 Linux c6h4o2-ASUS-TUF-Gaming-A15-FA507RM-FA507RM 7.0.0-28-generic #28~24.04.1-Ubuntu SMP PREEMPT_DYNAMIC Wed Jul  1 15:50:57 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  11:15:53 up  1:53,  1 user,  load average: 0.21, 0.30, 0.40

--- npc/vsrc/top.v
+++ npc/vsrc/top.v
@@ -3,22 +3,12 @@ module top(
   input rst,
   output reg[15:0] ledr
 );
-reg [31:0] cnt;
-always@(posedge clk)begin
-	if(rst)begin
-		cnt <= 0;
-		ledr <= 16'b1;
-	end
+  reg [31:0] count;
+    always @(posedge clk) begin
+	if (rst) begin led <= 1; count <= 0; end
 	else begin
-		cnt <= cnt + 1;
-		if(cnt >= 50000000)begin
-			if(ledr == 16'b1 << 15)
-				ledr <= 16'b1;
-			else if(ledr == 0)
-				ledr <= 16'b1;
-			else
-				ledr <= ledr << 1;
-		end
+	    if (count == 0) led <= {led[14:0], led[15]};
+	    count <= (count >= 5000000 ? 32'b0 : count + 1);
 	end
 end
 endmodule
