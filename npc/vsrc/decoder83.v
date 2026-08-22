3cad16a03c896a4e3f51257f9387c4bf9bba5e70
tracer-ysyx <tracer@ysyx.org>
2026-08-22 20:44:34 +0800

    > sim RTL ysyx_22040000 李晓琛 Linux c6h4o2-ASUS-TUF-Gaming-A15-FA507RM-FA507RM 7.0.0-28-generic #28~24.04.1-Ubuntu SMP PREEMPT_DYNAMIC Wed Jul  1 15:50:57 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  20:44:34 up 11:27,  1 user,  load average: 0.98, 1.18, 0.84

--- npc/vsrc/decoder83.v
+++ npc/vsrc/decoder83.v
@@ -1,19 +1,18 @@
-module decoder83(x,en,y);
+module decoder83(x,en,y,f);
   input  [7:0] x;
   input  en;
   output reg [2:0]y;
-  output f;
+  output reg f;
   integer i;
-  assign f = 0;
   always @(x or en) begin
     if (en) begin
-      y = 0;
+      y = 0;f = 0;
       for( i = 0; i <= 7; i = i+1)
           if(x[i] == 1)  begin
             y = i[2:0];
             f = 1;
           end
     end
-    else  y = 0;
+    else begin y = 0; f = 0; end
   end
 endmodule
