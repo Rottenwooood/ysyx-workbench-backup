2e16edb114f27a951da538dfa123f6e0ba5be03e
tracer-ysyx <tracer@ysyx.org>
2026-08-22 11:23:12 +0800

    > sim RTL ysyx_22040000 李晓琛 Linux c6h4o2-ASUS-TUF-Gaming-A15-FA507RM-FA507RM 7.0.0-28-generic #28~24.04.1-Ubuntu SMP PREEMPT_DYNAMIC Wed Jul  1 15:50:57 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  11:23:12 up  2:06,  1 user,  load average: 0.42, 0.57, 0.58

--- npc/csrc/main.cpp
+++ npc/csrc/main.cpp
@@ -1,60 +1,16 @@
-// DESCRIPTION: Verilator testbench for top.v (decode24)
-// Drives en/x and checks y, dumping an FST waveform.
+#include <nvboard.h>
+#include <Vtop.h>
 
-#include <verilated.h>
-#include "Vtop.h"
-#include "verilated_fst_c.h"
+static TOP_NAME dut;
 
-#include <cassert>
-#include <cstdio>
+void nvboard_bind_all_pins(TOP_NAME* top);
 
-static VerilatedContext* contextp = nullptr;
-static Vtop* top = nullptr;
-static VerilatedFstC* tfp = nullptr;
-static vluint64_t sim_time = 0;
+int main() {
+  nvboard_bind_all_pins(&dut);
+  nvboard_init();
 
-static void sim_init() {
-  contextp = new VerilatedContext;
-  contextp->traceEverOn(true);
-  top = new Vtop{contextp};
-  tfp = new VerilatedFstC;
-  top->trace(tfp, 99);
-  tfp->open("dump.fst");
-}
-
-static void step_and_dump_wave() {
-  top->eval();
-  tfp->dump(sim_time);
-  sim_time += 1;
-}
-
-static void sim_exit() {
-  top->final();
-  tfp->close();
-  delete top;
-  delete tfp;
-  delete contextp;
-}
-
-static void check() {
-  int expected = top->en ? (1 << top->x) : 0;
-  printf("en = %d, x = %02b, y = %04b\n", top->en, top->x, top->y);
-  assert(top->y == expected);
-}
-
-int main(int argc, char** argv) {
-  sim_init();
-
-  top->en = 0b0;  top->x = 0b00;  step_and_dump_wave();  check();
-                  top->x = 0b01;  step_and_dump_wave();  check();
-                  top->x = 0b10;  step_and_dump_wave();  check();
-                  top->x = 0b11;  step_and_dump_wave();  check();
-  top->en = 0b1;  top->x = 0b00;  step_and_dump_wave();  check();
-                  top->x = 0b01;  step_and_dump_wave();  check();
-                  top->x = 0b10;  step_and_dump_wave();  check();
-                  top->x = 0b11;  step_and_dump_wave();  check();
-
-  sim_exit();
-  printf("Test Passed!\n");
-  return 0;
+  while (1) {
+    nvboard_update();
+    dut.eval();
+  }
 }
