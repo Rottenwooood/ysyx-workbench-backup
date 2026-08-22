27a8735e806a9510da57365b2011b08b0c9a2ac6
tracer-ysyx <tracer@ysyx.org>
2026-08-22 20:32:00 +0800

    > sim RTL ysyx_22040000 李晓琛 Linux c6h4o2-ASUS-TUF-Gaming-A15-FA507RM-FA507RM 7.0.0-28-generic #28~24.04.1-Ubuntu SMP PREEMPT_DYNAMIC Wed Jul  1 15:50:57 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  20:32:00 up 11:14,  1 user,  load average: 1.09, 0.75, 0.53

--- npc/vsrc/top.v
+++ npc/vsrc/top.v
@@ -1,14 +1,17 @@
 module top(x,en,y);
-  input  [3:0] x;
+  input  [7:0] x;
   input  en;
-  output reg [1:0]y;
-  integer i;
-  always @(x or en) begin
-    if (en) begin
-      y = 0;
-      for( i = 0; i <= 3; i = i+1)
-          if(x[i] == 1)  y = i[1:0];
-    end
-    else  y = 0;
-  end
+  output reg [6:0]h;
+  output flag;
+  reg [2:0]y;
+  decoder83 u_dec83(
+	  .x (x),
+	  .en (en),
+	  .y (y),
+  )
+  bcd7seg u_seg(
+  	.b (y),
+	  .h (h),
+  )
+  
 endmodule
