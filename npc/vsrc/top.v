3b61617df0db5fe5b10741b66b870095327bf747
241220000-Li Xiaochen <2535771636@qq.com>
2026-08-25 23:15:31 +0800

    ysyx NJU 数字电路实验 实验八

--- npc/vsrc/top.v
+++ npc/vsrc/top.v
@@ -1,44 +1,35 @@
 module top(
-  input clk, resetn, ps2_clk, ps2_data,
-  output [6:0] led1,
-  output [6:0] led2,
-  output [6:0] led3,
-  output [6:0] led4,
-  output [6:0] led5,
-  output [6:0] led6
+  input clk,reset,
+  output VGA_CLK,
+  output VGA_HSYNC,
+  output VGA_VSYNC,
+  output VGA_BLANK_N,
+  output [7:0] VGA_R,
+  output [7:0] VGA_G,
+  output [7:0] VGA_B
 );
-reg [7:0] word_cnt;
-reg [9:0] buffer;
-wire [7:0] ascii;
-ps2_keyboard my_keyborad(clk,resetn,ps2_clk,ps2_data,word_cnt,buffer);
+assign VGA_CLK = clk;
 
-ascii_rom my_rom(
-    .clk  (clk),
-    .addr (buffer[8:1]),
-    .data (ascii)
-  );
-hex7seg my_led1(
-    .b (buffer[8:5]),
-    .h (led1)
-  );
-hex7seg my_led2(
-    .b (buffer[4:1]),
-    .h (led2)
-  );
-hex7seg my_led3(
-    .b (ascii[7:4]),
-    .h (led3)
-  );
-hex7seg my_led4(
-    .b (ascii[3:0]),
-    .h (led4)
-  );
-hex7seg my_led5(
-    .b (word_cnt[7:4]),
-    .h (led5)
-  );
-hex7seg my_led6(
-    .b (word_cnt[3:0]),
-    .h (led6)
-  );
+wire [9:0] h_addr;
+wire [9:0] v_addr;
+wire [23:0] vga_data;
+
+vga_ctrl my_vga_ctrl(
+    .pclk(clk),
+    .reset(reset),
+    .vga_data(vga_data),
+    .h_addr(h_addr),
+    .v_addr(v_addr),
+    .hsync(VGA_HSYNC),
+    .vsync(VGA_VSYNC),
+    .valid(VGA_BLANK_N),
+    .vga_r(VGA_R),
+    .vga_g(VGA_G),
+    .vga_b(VGA_B)
+);
+vmem my_vmem(
+    .h_addr(h_addr),
+    .v_addr(v_addr[8:0]),
+    .vga_data(vga_data)
+);
 endmodule
