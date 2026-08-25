22cebd7c11d213e08f81c70a8cabb7a94e63a1d8
tracer-ysyx <tracer@ysyx.org>
2026-08-25 21:29:40 +0800

    > sim RTL ysyx_22040000 李晓琛 Linux c6h4o2-ASUS-TUF-Gaming-A15-FA507RM-FA507RM 7.0.0-28-generic #28~24.04.1-Ubuntu SMP PREEMPT_DYNAMIC Wed Jul  1 15:50:57 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  21:29:40 up  8:46,  1 user,  load average: 0.78, 0.91, 1.01

--- npc/vsrc/top.v
+++ npc/vsrc/top.v
@@ -7,8 +7,14 @@ module top(
 );
 reg [7:0] word_cnt;
 reg [9:0] buffer;
+wire [7:0] ascii;
 ps2_keyboard my_keyborad(clk,resetn,ps2_clk,ps2_data,word_cnt,buffer);
 
+ascii_rom my_rom(
+    .clk  (clk),
+    .addr (buffer[8:1]),
+    .data (ascii)
+  );
 hex7seg my_led1(
     .b (buffer[8:5]),
     .h (led1)
@@ -18,10 +24,18 @@ hex7seg my_led2(
     .h (led2)
   );
 hex7seg my_led3(
-    .b (word_cnt[7:4]),
+    .b (ascii[7:4]),
     .h (led3)
   );
 hex7seg my_led4(
+    .b (ascii[3:0]),
+    .h (led4)
+  );
+hex7seg my_led5(
+    .b (word_cnt[7:4]),
+    .h (led3)
+  );
+hex7seg my_led6(
     .b (word_cnt[3:0]),
     .h (led4)
   );
