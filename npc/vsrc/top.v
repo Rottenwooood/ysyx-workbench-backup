2f41ee6a826cac33e73f83cd9504d106db23248e
tracer-ysyx <tracer@ysyx.org>
2026-08-25 20:54:27 +0800

    > sim RTL ysyx_22040000 李晓琛 Linux c6h4o2-ASUS-TUF-Gaming-A15-FA507RM-FA507RM 7.0.0-28-generic #28~24.04.1-Ubuntu SMP PREEMPT_DYNAMIC Wed Jul  1 15:50:57 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  20:54:27 up  8:11,  1 user,  load average: 0.59, 0.81, 0.82

--- npc/vsrc/top.v
+++ npc/vsrc/top.v
@@ -1,46 +1,28 @@
-module top
-(
-  input   clk, in, reset,
-  output reg [6:0] led,
-  output out
+module top(
+  input clk, resetn, ps2_clk, ps2_data,
+  output [6:0] led1,
+  output [6:0] led2,
+  output [6:0] led3,
+  output [6:0] led4
 );
+reg [7:0] word_cnt;
+reg [9:0] buffer;
+ps2_keyboard my_keyborad(clk,resetn,ps2_clk,ps2_data,word_cnt,buffer);
 
-parameter[3:0] S0 = 0, S1 = 1, S2 = 2, S3 = 3,
-          S4 = 4, S5 = 5, S6 = 6, S7 = 7, S8 = 8;
-
-wire [3:0] state_din, state_dout;
-wire state_wen;
-
-assign state_wen = 1;
-
-MuxKeyWithDefault#(9, 4, 1) outMux(.out(out), .key(state_dout), .default_out(0), .lut({
-  S0, 1'b0,
-  S1, 1'b0,
-  S2, 1'b0,
-  S3, 1'b0,
-  S4, 1'b1,
-  S5, 1'b0,
-  S6, 1'b0,
-  S7, 1'b0,
-  S8, 1'b1
-}));
-
-Reg #(4, S0) state_reg(.clk(clk), .rst(reset), .din(state_din), .dout(state_dout), .wen(state_wen));
-
-MuxKeyWithDefault#(9, 4, 4) stateMux(.out(state_din), .key(state_dout), .default_out(S0), .lut({
-  S0, in ? S5 : S1,
-  S1, in ? S5 : S2,
-  S2, in ? S5 : S3,
-  S3, in ? S5 : S4,
-  S4, in ? S5 : S4,
-  S5, in ? S6 : S1,
-  S6, in ? S7 : S1,
-  S7, in ? S8 : S1,
-  S8, in ? S8 : S1
-}));
-
-hex7seg led1(
-    .b (state_dout[3:0]),
-    .h (led)
+hex7seg my_led1(
+    .b (buffer[8:5]),
+    .h (led1)
+  );
+hex7seg my_led2(
+    .b (buffer[4:1]),
+    .h (led2)
+  );
+hex7seg my_led3(
+    .b (word_cnt[7:4]),
+    .h (led3)
+  );
+hex7seg my_led4(
+    .b (word_cnt[3:0]),
+    .h (led4)
   );
 endmodule
