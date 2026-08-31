b3d7a1f62278704367fb2b9f6a1f0d1134dd5daa
tracer-ysyx <tracer@ysyx.org>
2026-08-31 19:04:17 +0800

    > sim RTL ysyx_22040000 李晓琛 Linux c6h4o2-ASUS-TUF-Gaming-A15-FA507RM-FA507RM 7.0.0-28-generic #28~24.04.1-Ubuntu SMP PREEMPT_DYNAMIC Wed Jul  1 15:50:57 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  19:04:17 up 58 min,  1 user,  load average: 0.99, 1.73, 1.51

--- npc/csrc/main.cpp
+++ npc/csrc/main.cpp
@@ -101,21 +101,22 @@ int main(int argc, char *argv[]) {
     ref_inst_cycle();
 
     // DUT GPR: top.my_lsu.gpr.mem, see generated Vtop___024root.h
-    // uint32_t *dut_regs = dut.rootp->top__DOT__my_lsu__DOT__gpr__DOT__mem.data();
-    // uint32_t *ref_regs = ref_get_regs();
-    // uint32_t dut_pc = dut.rootp->top__DOT__pc_val;
-    // uint32_t ref_pc = ref_get_pc();
+    uint32_t *dut_regs = dut.rootp->top__DOT__my_lsu__DOT__gpr__DOT__mem.data();
+    uint32_t *ref_regs = ref_get_regs();
+    uint32_t dut_pc = dut.rootp->top__DOT__pc_val;
+    uint32_t ref_pc = ref_get_pc();
 
-    // int gpr_is_diff = check_regs(dut_regs, ref_regs);
-    // int pc_is_diff = check_pc(dut_pc, ref_pc);
-    // if (gpr_is_diff || pc_is_diff) {
-    //   printf("Simulation stop\n");
-    //   break;
-    // }
-    // if (++cycle > CYCLE_LIMIT) {
-    //   printf("cycle limit (%ld) reached, no diff found\n", CYCLE_LIMIT);
-    //   break;
-    // }
+    int gpr_is_diff = check_regs(dut_regs, ref_regs);
+    int pc_is_diff = check_pc(dut_pc, ref_pc);
+    if (gpr_is_diff || pc_is_diff) {
+      printf("Simulation stop\n");
+      break;
+    }
+    if (++cycle > CYCLE_LIMIT) {
+      printf("cycle limit (%ld) reached, no diff found\n", CYCLE_LIMIT);
+      break;
+    }
+    printf("1");
   }
 
   //
