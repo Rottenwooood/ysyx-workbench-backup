8f6dfbdca2536eff595518c03174e793ee1dc918
tracer-ysyx <tracer@ysyx.org>
2026-08-31 19:13:28 +0800

    > sim RTL ysyx_22040000 李晓琛 Linux c6h4o2-ASUS-TUF-Gaming-A15-FA507RM-FA507RM 7.0.0-28-generic #28~24.04.1-Ubuntu SMP PREEMPT_DYNAMIC Wed Jul  1 15:50:57 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  19:13:28 up  1:07,  1 user,  load average: 1.26, 1.07, 1.22

--- npc/csrc/main.cpp
+++ npc/csrc/main.cpp
@@ -99,7 +99,6 @@ int main(int argc, char *argv[]) {
     dut.inst = inst_fetch(dut.rootp->top__DOT__pc_val);
     dut_single_cycle();
     ref_inst_cycle();
-    printf("1");
 
     // DUT GPR: top.my_lsu.gpr.mem, see generated Vtop___024root.h
     uint32_t *dut_regs = dut.rootp->top__DOT__my_lsu__DOT__gpr__DOT__mem.data();
@@ -117,7 +116,6 @@ int main(int argc, char *argv[]) {
       printf("cycle limit (%ld) reached, no diff found\n", CYCLE_LIMIT);
       break;
     }
-    printf("1");
   }
 
   //
