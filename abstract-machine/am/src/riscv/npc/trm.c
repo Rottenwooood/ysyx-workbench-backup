34ad7f591adaa43b9d89e005fa12b5abebfb0692
tracer-ysyx <tracer@ysyx.org>
2026-08-31 20:37:19 +0800

    > sim RTL ysyx_22040000 李晓琛 Linux c6h4o2-ASUS-TUF-Gaming-A15-FA507RM-FA507RM 7.0.0-28-generic #28~24.04.1-Ubuntu SMP PREEMPT_DYNAMIC Wed Jul  1 15:50:57 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  20:37:19 up  2:31,  1 user,  load average: 1.20, 1.34, 1.19

--- abstract-machine/am/src/riscv/npc/trm.c
+++ abstract-machine/am/src/riscv/npc/trm.c
@@ -12,7 +12,7 @@ Area heap = RANGE(&_heap_start, PMEM_END);
 static const char mainargs[MAINARGS_MAX_LEN] = TOSTRING(MAINARGS_PLACEHOLDER); // defined in CFLAGS
 
 void putch(char ch) {
-  fputc(ch);
+  fputc(ch,stderr);
 }
 
 void halt(int code) {
