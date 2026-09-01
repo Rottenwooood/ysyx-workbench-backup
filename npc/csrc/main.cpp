fbbc0e0e3dbb9b263ce1b7207fe77b3ea7aaeba7
tracer-ysyx <tracer@ysyx.org>
2026-09-01 14:03:15 +0800

    > sim RTL ysyx_22040000 李晓琛 Linux c6h4o2-ASUS-TUF-Gaming-A15-FA507RM-FA507RM 7.0.0-28-generic #28~24.04.1-Ubuntu SMP PREEMPT_DYNAMIC Wed Jul  1 15:50:57 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  14:03:15 up  2:32,  1 user,  load average: 1.26, 1.46, 1.45

--- npc/csrc/main.cpp
+++ npc/csrc/main.cpp
@@ -20,8 +20,9 @@ struct timeval start, end;
 const unsigned long Converter = 1000 * 1000; // 1s == 1000 * 1000 us
 
 unsigned long long get_time() {
-  // int ret = gettimeofday(&end, NULL);
-  unsigned long long diff = cnt / (100 * 1000 * 100) * 1000 * 1000;
+  // 模拟时钟 100MHz: 1s = 100*1000*1000 = 1e8 个周期
+  // 返回从仿真开始经过的微秒数(按整秒取整)
+  unsigned long long diff = cnt / (100 * 1000 * 1000) * 1000 * 1000;
   return diff;
 }
 
